1a39a57b57bbb7e98418b98c4a2b26b9b3dcd996
Ryan Lee <ryanm.lee11@gmail.com>
2020-05-04 16:00:36 -0400

    Updates

---
+++ precache-manifest.39bfc03c15f4562d8574c055a0d6b2a8.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa8a3cc8ed19708f65625fcbe2d77e5",
+    "url": "/codenameRy/Premier-League-React.git/index.html"
+  },
+  {
+    "revision": "a42ce350c72fb093fe70",
+    "url": "/codenameRy/Premier-League-React.git/static/css/2.18f9a32c.chunk.css"
+  },
+  {
+    "revision": "c4d43a15738c914016e0",
+    "url": "/codenameRy/Premier-League-React.git/static/css/main.244bea7d.chunk.css"
+  },
+  {
+    "revision": "a42ce350c72fb093fe70",
+    "url": "/codenameRy/Premier-League-React.git/static/js/2.a0796704.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/codenameRy/Premier-League-React.git/static/js/2.a0796704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4d43a15738c914016e0",
+    "url": "/codenameRy/Premier-League-React.git/static/js/main.9a1a7656.chunk.js"
+  },
+  {
+    "revision": "c6b2943e010a246face3",
+    "url": "/codenameRy/Premier-League-React.git/static/js/runtime-main.8b644d25.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/codenameRy/Premier-League-React.git/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/codenameRy/Premier-League-React.git/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/codenameRy/Premier-League-React.git/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "8245771aaef1abfb5a9ecc074c1b239b",
+    "url": "/codenameRy/Premier-League-React.git/static/media/premier_league_trophy.8245771a.jpg"
+  },
+  {
+    "revision": "158cca255c5b1577dcd35d17a4840d64",
+    "url": "/codenameRy/Premier-League-React.git/static/media/premier_logo_large.158cca25.png"
+  }
+]);
