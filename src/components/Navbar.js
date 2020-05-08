d85c469694ba2f47fe5e9f568f66ca91b241504e
Ryan Lee <ryanm.lee11@gmail.com>
2020-05-07 20:16:19 -0400

    Responsive Navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,65 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import premierLargeLogo from './images/premier_logo_large.png'
+import premierLargeLogo from "./images/premier_logo_large.png";
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
 
-export default class Navbar extends Component {
+class Navbar extends Component {
   componentDidMount() {
     //window.onload happens once when the components first mounts
     console.log(this);
   }
-
   render() {
     return (
-      <nav className="navbar sticky-top navbar-light bg-white rounded-top">
-        <div className="container">
-          <div>
-          <img src={premierLargeLogo} height="30px" className="Ball" alt="football" />
-        </div>
-          <Link className="navbar" to="/Home">
-            <strong>Home</strong>
-          </Link>
-          <Link className="navbar" to="/SoccerTeams">
+      <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light bg-white ">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <Link className="nav-link" to="/Home">
+        <img src={premierLargeLogo} height="30px" className=" Ball" alt="football" />
+          </Link>        
+        
+
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item ">
+              <Link className="nav-link" to="/Home">
+              <strong>Home</strong> 
+                {/* <span className="sr-only">(current)</span> */}
+              </Link>
+            </li>
+            <li className="nav-item">
+            <Link className="nav-link" to="/SoccerTeams">
             <strong>Teams</strong>
           </Link>
-          <Link className="navbar" to="/VideoPlayer">
-            <strong>Videos</strong>
-          </Link>
-          <Link className="navbar" to="/History">
-            <strong>History</strong>
-          </Link>
-          <Link className="navbar" to="/About">
-            <strong>About</strong>
-          </Link>
-
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/VideoPlayer">
+                <strong>Videos</strong>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/History">
+              <strong>History</strong>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/About">
+              <strong>About</strong>
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     );
   }
 }
+
+export default Navbar;
